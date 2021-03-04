308cbf462b4d5a33deaa3e8f57705486b65902cf
Amit <Amit.kremer@appsflyer.com>
2021-03-04 16:00:43 +0200

    sanity

--- tests/tests.js
+++ tests/tests.js
@@ -3,26 +3,13 @@
 
 exports.defineAutoTests = function () {
 
-    describe('awesome tests', function () {
-        it('do something sync', function () {
+    describe('Simple sanity', function () {
+        it('this is success', function () {
             expect(1).toBe(1);
         });
 
-        it('do something async using callbacks', function (done) {
-            setTimeout(function () {
-                expect(1).toBe(1);
-                done();
-            }, 100);
-        });
-
-        it("do something async using promises", function () {
-            let value = 1;
-            expect(value).toBeGreaterThan(0);
-        });
-
-        it("do something async using async/await", async function () {
-            let value = 1;
-            expect(value).toBeGreaterThan(0);
+        it('this is failure', function () {
+            expect(1).toBe(2);
         });
     });
 };
